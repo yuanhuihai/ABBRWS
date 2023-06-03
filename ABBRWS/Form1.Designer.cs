41eee7e9a68a771601e9f37d504555178cf677b0
yuanhuihai <yuanhuihai@sina.com>
2023-06-03 20:56:35 +0800

    添加项目文件。

---
+++ ABBRWS/Form1.Designer.cs
@@ -0,0 +1,290 @@
+﻿namespace ABBRWS
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.LocalRegist = new System.Windows.Forms.Button();
+            this.mShipGet = new System.Windows.Forms.Button();
+            this.jogAxisModeSet = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.jogOneAdd = new System.Windows.Forms.Button();
+            this.jogOneMinus = new System.Windows.Forms.Button();
+            this.axisTwoMinus = new System.Windows.Forms.Button();
+            this.axisTwoAdd = new System.Windows.Forms.Button();
+            this.axisThreeMinus = new System.Windows.Forms.Button();
+            this.axisThreeAdd = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.button12 = new System.Windows.Forms.Button();
+            this.button13 = new System.Windows.Forms.Button();
+            this.button14 = new System.Windows.Forms.Button();
+            this.button15 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // LocalRegist
+            // 
+            this.LocalRegist.Location = new System.Drawing.Point(110, 48);
+            this.LocalRegist.Name = "LocalRegist";
+            this.LocalRegist.Size = new System.Drawing.Size(116, 23);
+            this.LocalRegist.TabIndex = 0;
+            this.LocalRegist.Text = "注册本地用户";
+            this.LocalRegist.UseVisualStyleBackColor = true;
+            this.LocalRegist.Click += new System.EventHandler(this.LocalRegist_Click);
+            // 
+            // mShipGet
+            // 
+            this.mShipGet.Location = new System.Drawing.Point(110, 97);
+            this.mShipGet.Name = "mShipGet";
+            this.mShipGet.Size = new System.Drawing.Size(116, 23);
+            this.mShipGet.TabIndex = 1;
+            this.mShipGet.Text = "请求Motion权限";
+            this.mShipGet.UseVisualStyleBackColor = true;
+            this.mShipGet.Click += new System.EventHandler(this.mShipGet_Click);
+            // 
+            // jogAxisModeSet
+            // 
+            this.jogAxisModeSet.Location = new System.Drawing.Point(110, 145);
+            this.jogAxisModeSet.Name = "jogAxisModeSet";
+            this.jogAxisModeSet.Size = new System.Drawing.Size(116, 23);
+            this.jogAxisModeSet.TabIndex = 2;
+            this.jogAxisModeSet.Text = "设置单轴模式";
+            this.jogAxisModeSet.UseVisualStyleBackColor = true;
+            this.jogAxisModeSet.Click += new System.EventHandler(this.jogAxisModeSet_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button14);
+            this.groupBox1.Controls.Add(this.button15);
+            this.groupBox1.Controls.Add(this.button12);
+            this.groupBox1.Controls.Add(this.button13);
+            this.groupBox1.Controls.Add(this.button10);
+            this.groupBox1.Controls.Add(this.button11);
+            this.groupBox1.Controls.Add(this.axisThreeMinus);
+            this.groupBox1.Controls.Add(this.axisThreeAdd);
+            this.groupBox1.Controls.Add(this.axisTwoMinus);
+            this.groupBox1.Controls.Add(this.axisTwoAdd);
+            this.groupBox1.Controls.Add(this.jogOneMinus);
+            this.groupBox1.Controls.Add(this.jogOneAdd);
+            this.groupBox1.Location = new System.Drawing.Point(269, 48);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(225, 208);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "单轴运动";
+            // 
+            // jogOneAdd
+            // 
+            this.jogOneAdd.Location = new System.Drawing.Point(29, 29);
+            this.jogOneAdd.Name = "jogOneAdd";
+            this.jogOneAdd.Size = new System.Drawing.Size(75, 23);
+            this.jogOneAdd.TabIndex = 0;
+            this.jogOneAdd.Text = "1轴 +";
+            this.jogOneAdd.UseVisualStyleBackColor = true;
+            this.jogOneAdd.MouseDown += new System.Windows.Forms.MouseEventHandler(this.jogOneAdd_MouseDown);
+            this.jogOneAdd.MouseUp += new System.Windows.Forms.MouseEventHandler(this.jogOneAdd_MouseUp);
+            // 
+            // jogOneMinus
+            // 
+            this.jogOneMinus.Location = new System.Drawing.Point(123, 29);
+            this.jogOneMinus.Name = "jogOneMinus";
+            this.jogOneMinus.Size = new System.Drawing.Size(75, 23);
+            this.jogOneMinus.TabIndex = 1;
+            this.jogOneMinus.Text = "1轴 -";
+            this.jogOneMinus.UseVisualStyleBackColor = true;
+            this.jogOneMinus.MouseDown += new System.Windows.Forms.MouseEventHandler(this.jogOneMinus_MouseDown);
+            this.jogOneMinus.MouseUp += new System.Windows.Forms.MouseEventHandler(this.jogOneMinus_MouseUp);
+            // 
+            // axisTwoMinus
+            // 
+            this.axisTwoMinus.Location = new System.Drawing.Point(123, 58);
+            this.axisTwoMinus.Name = "axisTwoMinus";
+            this.axisTwoMinus.Size = new System.Drawing.Size(75, 23);
+            this.axisTwoMinus.TabIndex = 3;
+            this.axisTwoMinus.Text = "2轴 -";
+            this.axisTwoMinus.UseVisualStyleBackColor = true;
+            this.axisTwoMinus.MouseDown += new System.Windows.Forms.MouseEventHandler(this.axisTwoMinus_MouseDown);
+            this.axisTwoMinus.MouseUp += new System.Windows.Forms.MouseEventHandler(this.axisTwoMinus_MouseUp);
+            // 
+            // axisTwoAdd
+            // 
+            this.axisTwoAdd.Location = new System.Drawing.Point(29, 58);
+            this.axisTwoAdd.Name = "axisTwoAdd";
+            this.axisTwoAdd.Size = new System.Drawing.Size(75, 23);
+            this.axisTwoAdd.TabIndex = 2;
+            this.axisTwoAdd.Text = "2轴 +";
+            this.axisTwoAdd.UseVisualStyleBackColor = true;
+            this.axisTwoAdd.MouseDown += new System.Windows.Forms.MouseEventHandler(this.axisTwoAdd_MouseDown);
+            this.axisTwoAdd.MouseUp += new System.Windows.Forms.MouseEventHandler(this.axisTwoAdd_MouseUp);
+            // 
+            // axisThreeMinus
+            // 
+            this.axisThreeMinus.Location = new System.Drawing.Point(123, 87);
+            this.axisThreeMinus.Name = "axisThreeMinus";
+            this.axisThreeMinus.Size = new System.Drawing.Size(75, 23);
+            this.axisThreeMinus.TabIndex = 5;
+            this.axisThreeMinus.Text = "3轴 -";
+            this.axisThreeMinus.UseVisualStyleBackColor = true;
+            // 
+            // axisThreeAdd
+            // 
+            this.axisThreeAdd.Location = new System.Drawing.Point(29, 87);
+            this.axisThreeAdd.Name = "axisThreeAdd";
+            this.axisThreeAdd.Size = new System.Drawing.Size(75, 23);
+            this.axisThreeAdd.TabIndex = 4;
+            this.axisThreeAdd.Text = "3轴 +";
+            this.axisThreeAdd.UseVisualStyleBackColor = true;
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(123, 116);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(75, 23);
+            this.button10.TabIndex = 7;
+            this.button10.Text = "4轴 -";
+            this.button10.UseVisualStyleBackColor = true;
+            // 
+            // button11
+            // 
+            this.button11.Location = new System.Drawing.Point(29, 116);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(75, 23);
+            this.button11.TabIndex = 6;
+            this.button11.Text = "4轴 +";
+            this.button11.UseVisualStyleBackColor = true;
+            // 
+            // button12
+            // 
+            this.button12.Location = new System.Drawing.Point(123, 145);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(75, 23);
+            this.button12.TabIndex = 9;
+            this.button12.Text = "5轴 -";
+            this.button12.UseVisualStyleBackColor = true;
+            // 
+            // button13
+            // 
+            this.button13.Location = new System.Drawing.Point(29, 145);
+            this.button13.Name = "button13";
+            this.button13.Size = new System.Drawing.Size(75, 23);
+            this.button13.TabIndex = 8;
+            this.button13.Text = "5轴 +";
+            this.button13.UseVisualStyleBackColor = true;
+            // 
+            // button14
+            // 
+            this.button14.Location = new System.Drawing.Point(123, 174);
+            this.button14.Name = "button14";
+            this.button14.Size = new System.Drawing.Size(75, 23);
+            this.button14.TabIndex = 5;
+            this.button14.Text = "6轴 -";
+            this.button14.UseVisualStyleBackColor = true;
+            // 
+            // button15
+            // 
+            this.button15.Location = new System.Drawing.Point(29, 174);
+            this.button15.Name = "button15";
+            this.button15.Size = new System.Drawing.Size(75, 23);
+            this.button15.TabIndex = 4;
+            this.button15.Text = "6轴 +";
+            this.button15.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(45, 53);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 12);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "步骤1：";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(45, 102);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 12);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "步骤2：";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(45, 150);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(47, 12);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "步骤3：";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(679, 351);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.jogAxisModeSet);
+            this.Controls.Add(this.mShipGet);
+            this.Controls.Add(this.LocalRegist);
+            this.Name = "Form1";
+            this.Text = "  C#基于robot web servcie和ABB机器人通讯";
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button LocalRegist;
+        private System.Windows.Forms.Button mShipGet;
+        private System.Windows.Forms.Button jogAxisModeSet;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button14;
+        private System.Windows.Forms.Button button15;
+        private System.Windows.Forms.Button button12;
+        private System.Windows.Forms.Button button13;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Button axisThreeMinus;
+        private System.Windows.Forms.Button axisThreeAdd;
+        private System.Windows.Forms.Button axisTwoMinus;
+        private System.Windows.Forms.Button axisTwoAdd;
+        private System.Windows.Forms.Button jogOneMinus;
+        private System.Windows.Forms.Button jogOneAdd;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ ABBRWS/Form1.cs
@@ -0,0 +1,282 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Timers;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+
+namespace ABBRWS
+{
+    public partial class Form1 : Form
+    {
+
+        private System.Timers.Timer timer1 = new System.Timers.Timer();
+        private System.Timers.Timer timer2 = new System.Timers.Timer();
+        private System.Timers.Timer timer3 = new System.Timers.Timer();
+        private System.Timers.Timer timer4 = new System.Timers.Timer();
+
+
+        private CookieContainer _cookies = new CookieContainer();
+
+        public Form1()
+        {
+            this.timer1.Elapsed += timer1_Tick;
+            this.timer2.Elapsed += timer2_Tick;
+            this.timer3.Elapsed += timer3_Tick;
+            this.timer4.Elapsed += timer4_Tick;
+            InitializeComponent();
+        }
+
+
+        private void LocalRegist_Click(object sender, EventArgs e)
+        {
+
+            string url = $"http://127.0.0.1/users";
+            string body = "username=xyz&application=RobotStudio&location=IN-BLR-XXXX&ulocale=local";
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            request.Method = "POST";
+            request.Credentials = new NetworkCredential("Default User", "robotics");
+            request.ContentType = "application/x-www-form-urlencoded";
+            request.CookieContainer = _cookies;
+            Stream s = request.GetRequestStream();
+            s.Write(Encoding.ASCII.GetBytes(body), 0, body.Length);//向数据流中写入body 内容
+            s.Close();
+            using (var a = (HttpWebResponse)request.GetResponse())
+            {
+                if (a.StatusCode == HttpStatusCode.Created)
+                {
+                    Console.WriteLine("Created");
+                }
+
+            }
+
+        }
+
+        private void mShipGet_Click(object sender, EventArgs e)
+        {
+            string url = $"http://127.0.0.1/rw/mastership/motion?action=request";//请求权限URL
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            request.Method = "POST";
+            request.Credentials = new NetworkCredential("Default User", "robotics");
+            request.ContentType = "application/x-www-form-urlencoded";
+            request.CookieContainer = _cookies;
+            Stream s = request.GetRequestStream();
+            s.Close();
+            using (var httpResponse = (HttpWebResponse)request.GetResponse())
+                if (httpResponse.StatusCode == HttpStatusCode.NoContent)
+                {
+                    Console.WriteLine("Motion response:NO_CONTENT");
+                }
+
+        }
+
+        private void jogAxisModeSet_Click(object sender, EventArgs e)
+        {
+            string url = $"http://127.0.0.1/rw/motionsystem/mechunits/ROB_1?action=set&continue-on-err=1";
+            string body = "jog-mode=AxisGroup1"; //运动模式为单轴运动 
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            request.Method = "POST";
+            request.Credentials = new NetworkCredential("Default User", "tobotics");
+            request.CookieContainer = _cookies;
+            request.ContentType = "application / x - www - form - uriencoded";
+            Stream s = request.GetRequestStream();
+            s.Write(Encoding.ASCII.GetBytes(body), 0, body.Length);
+            s.Close();
+            using (var httpResponse = (HttpWebResponse)request.GetResponse())
+            {
+                if (httpResponse.StatusCode == HttpStatusCode.NoContent)
+                {
+                    Console.Write("Jog Mode Set Response:NO_CONTENT");
+                }
+            }
+        }
+
+        private int getCCount()
+        {
+            string url = $"http://127.0.0.1/rw/motionsystem?resource=change-count&json=1";
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            request.Method = "GET";
+            request.Credentials = new NetworkCredential("Default User", "robotics");
+            request.CookieContainer = _cookies;
+            request.Proxy = null;
+            request.Timeout = 10000;
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            if (response != null)
+            {
+                using (StreamReader reader = new StreamReader(response.GetResponseStream()))
+                {
+                    string content = reader.ReadToEnd();
+                    dynamic res = JsonConvert.DeserializeObject(content);
+                    var state = res._embedded._state[0];
+                    if (state != null)
+                    {
+                        Console.WriteLine("state:" + state["change-count"]);
+                        return Convert.ToInt32(state["change-count"]);//返回当前 CCount
+                    }
+                }
+               
+              
+            }
+        
+            return -1;
+        }
+
+
+        private void timer1_Tick(object sender,ElapsedEventArgs e)
+        {
+            timer1.Stop();
+            int ccount = getCCount();//获取当前CCount
+            string url = $"http://127.0.0.1/rw/motionsystem?action=jog";
+            string body = "axis1=1000&axis2=0&axis3=0&axis4=0&axis5=0&axis6=0&ccount=" + ccount + "";//此处举例1轴正向运动，速度矢量为1000
+            HttpWebRequest request = (HttpWebRequest) WebRequest.Create(url);
+            request.Method = "POST";
+            request.Credentials = new NetworkCredential("Default User", "robotics");
+            request.CookieContainer = _cookies;
+            request.ContentType = "application/x-www-form-urlencoded";
+            Stream s = request.GetRequestStream();
+            s.Write(Encoding.ASCII.GetBytes(body),0, body.Length);
+            s.Close();
+            using (var httpResponse = (HttpWebResponse)request.GetResponse())
+            {
+            }
+            timer1.Start();
+
+
+        }
+        private void timer2_Tick(object sender, ElapsedEventArgs e)
+        {
+            timer2.Stop();
+            int ccount = getCCount();//获取当前CCount
+            string url = $"http://127.0.0.1/rw/motionsystem?action=jog";
+            string body = "axis1=-1000&axis2=0&axis3=0&axis4=0&axis5=0&axis6=0&ccount=" + ccount + "";//此处举例1轴负向运动，速度矢量为-1000
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            request.Method = "POST";
+            request.Credentials = new NetworkCredential("Default User", "robotics");
+            request.CookieContainer = _cookies;
+            request.ContentType = "application/x-www-form-urlencoded";
+            Stream s = request.GetRequestStream();
+            s.Write(Encoding.ASCII.GetBytes(body), 0, body.Length);
+            s.Close();
+            using (var httpResponse = (HttpWebResponse)request.GetResponse())
+            {
+            }
+            timer2.Start();
+
+
+        }
+
+        private void timer3_Tick(object sender, ElapsedEventArgs e)
+        {
+            timer3.Stop();
+            int ccount = getCCount();//获取当前CCount
+            string url = $"http://127.0.0.1/rw/motionsystem?action=jog";
+            string body = "axis1=0&axis2=1000&axis3=0&axis4=0&axis5=0&axis6=0&ccount=" + ccount + "";//此处举例2轴正向运动，速度矢量为1000
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            request.Method = "POST";
+            request.Credentials = new NetworkCredential("Default User", "robotics");
+            request.CookieContainer = _cookies;
+            request.ContentType = "application/x-www-form-urlencoded";
+            Stream s = request.GetRequestStream();
+            s.Write(Encoding.ASCII.GetBytes(body), 0, body.Length);
+            s.Close();
+            using (var httpResponse = (HttpWebResponse)request.GetResponse())
+            {
+            }
+            timer3.Start();
+
+
+        }
+        private void timer4_Tick(object sender, ElapsedEventArgs e)
+        {
+            timer4.Stop();
+            int ccount = getCCount();//获取当前CCount
+            string url = $"http://127.0.0.1/rw/motionsystem?action=jog";
+            string body = "axis1=0&axis2=-1000&axis3=0&axis4=0&axis5=0&axis6=0&ccount=" + ccount + "";//此处举例1轴负向运动，速度矢量为-1000
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            request.Method = "POST";
+            request.Credentials = new NetworkCredential("Default User", "robotics");
+            request.CookieContainer = _cookies;
+            request.ContentType = "application/x-www-form-urlencoded";
+            Stream s = request.GetRequestStream();
+            s.Write(Encoding.ASCII.GetBytes(body), 0, body.Length);
+            s.Close();
+            using (var httpResponse = (HttpWebResponse)request.GetResponse())
+            {
+            }
+            timer4.Start();
+
+
+        }
+
+
+        private void jogOneAdd_MouseDown(object sender, MouseEventArgs e)
+        {
+            this.timer1.Interval = 200;
+            this.timer1.Enabled = true;
+        }
+
+        private void jogOneAdd_MouseUp(object sender, MouseEventArgs e)
+        {
+            this.timer1.Stop();
+            this.PerformJogStop();
+        }
+
+        public void PerformJogStop()
+        {
+            string url=$"http://127.0.0.1/rw/motionsystem?action=jog";
+            int ccount = getCCount();
+            string body = "axis1=0&axis2=0&axis3=0&axis4=0&axis5=0&axis6=0&ccount=" + ccount + "";
+            HttpWebRequest request=(HttpWebRequest)WebRequest.Create(url);
+            request.Method = "POST";
+            request.Credentials = new NetworkCredential("Default User", "robotics");
+            request.CookieContainer=_cookies;
+            request.ContentType ="application/x-www-form-urlencoded";
+            Stream s = request.GetRequestStream();
+            s.Write(Encoding.ASCII.GetBytes(body), 0, body.Length);
+            s.Close();
+            using (var httpResponse = (HttpWebResponse)request.GetResponse())
+            {
+            }
+        }
+
+        private void jogOneMinus_MouseDown(object sender, MouseEventArgs e)
+        {
+            this.timer2.Interval = 200;
+            this.timer2.Enabled = true;
+        }
+
+        private void jogOneMinus_MouseUp(object sender, MouseEventArgs e)
+        {
+            this.timer2.Stop();
+            this.PerformJogStop();
+        }
+
+        private void axisTwoAdd_MouseDown(object sender, MouseEventArgs e)
+        {
+            this.timer3.Interval = 200;
+            this.timer3.Enabled = true;
+       
+        }
+
+        private void axisTwoAdd_MouseUp(object sender, MouseEventArgs e)
+        {
+            this.timer3.Stop();
+            this.PerformJogStop();
+        }
+
+        private void axisTwoMinus_MouseDown(object sender, MouseEventArgs e)
+        {
+            this.timer4.Interval = 200;
+            this.timer4.Enabled = true;
+        }
+
+        private void axisTwoMinus_MouseUp(object sender, MouseEventArgs e)
+        {
+            this.timer4.Stop();
+            this.PerformJogStop();
+        }
+    }
+}
